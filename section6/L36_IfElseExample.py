dbd78e38e7e253dc66d2f268027bbe68ccb9945a
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-02-23 23:47:03 +0700

    Add

---
+++ section6/L36_IfElseExample.py
@@ -0,0 +1,7 @@
+money = 1000
+if money > 300:
+    print("Go to KFCCC !!!")
+else:
+    print("So Sad !!!")
+
+print("Bye Bye")
---
+++ section6/L37_LoginExample.py
@@ -0,0 +1,7 @@
+usernameInput = input("username: ")
+passwordInput = input("Password: ")
+
+if usernameInput == "admin" and passwordInput == "123":
+    print("Done !!")
+else:
+    print("ERROR!!!")
